8bceb4023b404ab955793c3d5e6ded2e17519592
Naphatnan Sillapa <126888826+GameNPN@users.noreply.github.com>
2024-02-12 13:14:32 +0700

    Week7-unscucess2

---
+++ Week 8/Web01/script.js
@@ -0,0 +1,25 @@
+"use strict";
+const countries = document.querySelector(".countries");
+
+const getCountry = function (country) {
+  console.log(country);
+  const req = new XMLHttpRequest();
+  req.open("GET", `https://restcountries.com/v3.1/name/${country} `);
+  req.send();
+
+  req.addEventListener("load", function () {
+    console.log(this.responseText);
+    const [data] = JSON.parse(this.responseText)
+    const html = ` <article class="country">
+  <img class="country_img" src="${data.flags.png}" alt="TH" />
+  <div class="country_data">
+    <h3 class="country_name">${data.name.common}</h3>
+    <h4 class="country_region">${data.region}</h4>
+  </div>
+  </article>`;
+    countries.insertAdjacentHTML("beforeend", html);
+    countries.style.opacity = 1
+  });
+};
+
+getCountry("Saudi Arabia");
