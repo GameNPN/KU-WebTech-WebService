81facf91b6bffe2a8362fd3ef9d92633bd163bc3
Naphatnan Sillapa <126888826+GameNPN@users.noreply.github.com>
2024-02-12 14:05:51 +0700

    Week7-unscucess3

--- Week 8/Web01/script.js
+++ Week 8/Web01/script.js
@@ -10,16 +10,30 @@ const getCountry = function (country) {
   req.addEventListener("load", function () {
     console.log(this.responseText);
     const [data] = JSON.parse(this.responseText)
+    const languagesHTML = Object.values(data.languages).map(lang => lang);
+    const currenciesnameHTML = Object.values(data.currencies).map(currency => currency.name);
+    const currenciessymbolHTML = Object.values(data.currencies).map(currency => currency.symbol);
+    console.log(currenciesnameHTML);
     const html = ` <article class="country">
   <img class="country_img" src="${data.flags.png}" alt="TH" />
   <div class="country_data">
     <h3 class="country_name">${data.name.common}</h3>
     <h4 class="country_region">${data.region}</h4>
+    <p class = "country_row"><span>🗣️ ${languagesHTML}</span></p>
+    <p class = "country_row">🪙 ${
+      Object.values(data.currencies).map((x) => `<span>${x.name} (${x.symbol})</span><br>`)
+    }
   </div>
+  
   </article>`;
     countries.insertAdjacentHTML("beforeend", html);
     countries.style.opacity = 1
   });
 };
 
+getCountry("Hong Kong");
 getCountry("Saudi Arabia");
+getCountry("Cook Islands");
+getCountry("Japan");
+getCountry("thailand");
+getCountry("lao");
